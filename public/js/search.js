2c4fcca777869c78c638b6a4bd4c095cfddbdf07
Scout <ricardog.navi20@gmail.com>
2022-11-30 19:59:28 -0600

    Added events

--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,8 @@ eventCTemplate = document.querySelector("[data-event-template]");
 eventCardContainer = document.querySelector("[data-event-cards-container]");
 searchInput = document.querySelector("[data-search]");
 
+console.log(eventCTemplate, eventCardContainer, searchInput);
+
 let events = [];
 
 searchInput.addEventListener("input", (e) => {
@@ -17,14 +19,17 @@ fetch("http://localhost:3000/api/event")
   .then((res) => res.json())
   .then(({ data }) => {
     console.log(data);
-    events = data.map((event) => {
+    events = data.map((event, idx) => {
       const card = eventCTemplate.content.cloneNode(true).children[0];
       const title = card.querySelector("[data-title]");
       const text = card.querySelector("[data-text]");
       title.textContent = event.title;
       text.textContent = event.text;
-      console.log(card);
+      console.log(idx, card);
+      card.id = idx;
       eventCardContainer.append(card);
+      $(`.event-row#${idx} .card-title`)[0].outerText = event.name;
+      $(`.event-row#${idx} .card-text`)[0].outerText = event.ubication;
       return { title: event.title, text: event.text, element: card };
     });
   });
